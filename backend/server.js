61cf234bd2fdfce6a4fb6f86e4b1587cdc7c3588
Sanjana Neware <newaress08@gmail.com>
2025-09-25 13:04:07 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const { mongodbConnection } = config;
 const { User } = models;
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: 'http://localhost:3000',
